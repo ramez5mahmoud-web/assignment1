b7307c02650f421c8fced2b624ac8f5a95b9a642
Ramez Alawneh <ramez5mahmoud@gmail.com>
2026-02-09 20:20:30 +0300

    assignment

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter amount");
+double amount = Convert.ToDouble(Console.ReadLine());
 
+
+if (amount < 0)
+{
+    Console.WriteLine($"invalid amount : {amount}");
+
+}
+else if (amount >= 500)
+{
+    double discountamount = (amount * 0.2);
+    double finalamount = amount - discountamount;
+
+}
+else if (amount < 500 && amount >= 300)
+{
+    double discountamount = amount * 0.2;
+    double finalamount = amount - discountamount;
+    Console.WriteLine($"the final amount after discount (10%) : {finalamount}");
+
+}
+else
+{
+    Console.WriteLine($"the the final amount : {amount}");
+}
